783a9a2bd3727ea9e6a9579828497eb51c75ebe1
SafitriNur <nursafitri6692@gmail.com>
2020-02-11 18:48:48 +0700

    Linear Layout

---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/LinearLayout.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LinearLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_linear_layout);
+    }
+}
---
+++ ConstraintLayout/app/src/main/java/id/ac/polinema/constraintlayout/RelatifLayout.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.constraintlayout;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class RelatifLayout extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_relatif_layout);
+    }
+}
