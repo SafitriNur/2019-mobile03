b844d29d19dd0f678ad93422e691d7156fb861d0
SafitriNur <nursafitri6692@gmail.com>
2020-02-11 13:43:04 +0700

    halaman login successful

---
+++ Aplikasiku/app/src/main/java/id/ac/polinema/aplikasiku/SuccessLoginPasswordActivity.java
@@ -0,0 +1,14 @@
+package id.ac.polinema.aplikasiku;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SuccessLoginPasswordActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success_login_password);
+    }
+}
